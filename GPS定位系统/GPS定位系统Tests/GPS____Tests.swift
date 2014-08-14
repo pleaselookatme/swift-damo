3c90340dbbe9d2bc11a8d5e412a6bc90466fd539
李小刚 <15005172190@163.com>
2014-08-14 17:38:06 +0800

    no message

---
+++ GPS定位系统/GPS定位系统Tests/GPS____Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GPS____Tests.swift
+//  GPS定位系统Tests
+//
+//  Created by mohoo on 14-8-14.
+//  Copyright (c) 2014年 mohoo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GPS____Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
